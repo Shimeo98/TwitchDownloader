d9f21df0b4e63a1cc9a88a4ab39d21eb1755dcb0
Joe Shimell <49585339+JoeShimell@users.noreply.github.com>
2021-03-21 01:15:13 +0000

    start on parsing youtube comments from chat-downloader

---
+++ TwitchDownloaderCore/YoutubeObjects/ChatRootYT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class ChatRootYT
+{
+    public Author author { get; set; }
+    public String message { get; set; }
+    public double time_in_seconds { get; set; }
+}
+
+public class MessageContainer
+{
+
+}
+
+public class Author
+{
+    public String name { get; set; }
+}
---
+++ TwitchDownloaderCore/YoutubeObjects/YoutubeComment.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class YoutubeComment
+{
+    public string Section { get; set; }
+    public double SecondsOffset { get; set; }
+}
